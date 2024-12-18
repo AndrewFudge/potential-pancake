d8a051d4bd914016d5da7fd1bebe4849255c3644
AndrewFudge <rudge@gmx.co.uk>
2024-12-18 17:06:17 +0000

    started to create a linked list

--- OOP/linked_list.py
+++ OOP/linked_list.py
@@ -7,8 +7,32 @@ class Node:
         return self.data
 
 class LinkedList:
-    def __init__(self):
+    def __init__(self, nodes=None):
         self.head = None
+        if nodes is not None:
+            node = Node(data=nodes.pop(0))
+            self.head = node
+            for each in nodes:
+                node.next = Node(data=each)
+                node = node.next
+        
+    def add_first(self, node):
+        node.next = self.head
+        self.head = node
+
+        def add_last(self, node):
+            if not self.head:
+                self.head = node
+                return
+            for current_node in self:
+                pass
+            current_node.next = node
+
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
 
     def __repr__(self):
         node = self.head
@@ -16,5 +40,5 @@ class LinkedList:
         while node is not None:
             nodes.append(node.data)
             node = node.next
-        node.append("None")
+        nodes.append("None")
         return " -> ".join(nodes)
